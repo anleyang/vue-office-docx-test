ed8b6a4cba45f61f90fb11c146d9ff42c9a861c6
anleyang <1976696799@qq.com>
2024-10-09 08:57:58 +0800

    Initial commit

---
+++ src/components/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <vue-office-docx
+    :src="docx"
+    style="height: 100vh;"
+    @rendered="rendered"
+  />
+</template>
+
+<script>
+//引入VueOfficeDocx组件
+import VueOfficeDocx from '@vue-office/docx'
+//引入相关样式
+import '@vue-office/docx/lib/index.css'
+
+export default {
+  components: {
+    VueOfficeDocx
+  },
+  data() {
+    return {
+      docx: 'http://static.shanhuxueyuan.com/test6.docx' 
+    }
+  },
+  methods: {
+    rendered() {
+      console.log("渲染完成")
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* 添加必要的样式调整 */
+</style>
